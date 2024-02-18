ad9022d249ea740d52df6b00d3b67c45fb930dd7
xtw33kky <daregood02@gmail.com>
2024-02-18 20:40:42 +0300

    Revert "Update main.cpp"
    This reverts commit 74fe6e75f0e36979c111cf90b15d2797bbf3d367.

--- main.cpp
+++ main.cpp
@@ -13,21 +13,13 @@ int main()
 	int input = 1;
 	while (input != -1)
 	{
-		system("cls");
 		std::cout << "Введите целое положительное число от 0 до 20 или -1 для выхода\n";
-		std::cin >> input;
-		
-		if (std::cin.fail())
-		{
-			std::cout << "Ошибка ввода данных." << std::endl;
-		}
-		else
-		{
-			if (input >= 0 && input <= 20)
-				std::cout << "Факториал равен " << findFact(input) << "\n\n";
-			else
-				std::cout << "Ошибка ввода данных." << std::endl;
-		}
+		std::cin.clear();
+		std::cin.sync();
+		if (std::cin >> input && input >= 0 && input <= 20)
+			std::cout << "Факториал равен " << findFact(input) << "\n\n";
+		else if (input != -1)
+			std::cout << "Некорректный ввод\n\n";
 	}
 	std::cout << "До свидания\n";
 	return 0;
